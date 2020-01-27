7d8d649f487cf857fc169248958c95d03aba87d6
github username <github email address>
2020-01-26 20:26:09 -0500

    pause

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+let element = document.createElement('div') 
+document.body.appendChild(element)
+
+let ul = document.createElement('ul')
+
+for (let i = 0; i < 3; 1++) { 
+    let li = document.createElement('li')
+    li.innerHTML = (i + 1).toString()
+    ul.appendChild(li)
+}
+
+element.appendChild(ul)
+
+let element = document.querySelector("p#greeting");
+element.innerHTML = 'Hello, Dom!'
